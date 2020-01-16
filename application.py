1245a72dc20d22f9c35e14658c116adc84bd797a
rikj26 <rikj26@gmail.com>
2020-01-16 12:31:27 +0000

    register verbeterd

--- application.py
+++ application.py
@@ -55,6 +55,11 @@ def index():
     return apology("persoonlijke homepage")
 
 
+@app.route("/check", methods=["GET"])
+def check():
+
+
+
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
@@ -70,14 +75,6 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
